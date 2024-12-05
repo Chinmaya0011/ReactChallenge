fc6f55f9ad142aa5ef6a53d1ca256b51dd282060
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-12-06 01:23:08 +0530

    update

--- Day1/arrFlat.js
+++ Day1/arrFlat.js
@@ -1,12 +1,14 @@
 let arr=[[1,2],[3,4],[5,6],[7,8],[9,0,1]];
 
-const flatArr=(arr)=>{
-    let result=[];
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr[i].length;j++){
-            result.push(arr[i][j])
-        }
-    }
-    return result;
-}
-console.log(flatArr(arr))
+// const flatArr=(arr)=>{
+//     let result=[];
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=0;j<arr[i].length;j++){
+//             result.push(arr[i][j])
+//         }
+//     }
+//     return result;
+// }
+// console.log(flatArr(arr))
+
+console.log(arr.flat())
